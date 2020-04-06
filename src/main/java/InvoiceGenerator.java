c22847368af20e441a8406b77e35ac4272c7b998
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-06 20:00:05 +0530

    REFACTORED : Code to return minimum fare in case less distance and time provided

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,7 +1,9 @@
 public class InvoiceGenerator {
     private static final double RATE_PER_KM = 10;
     private static final int RATE_PER_MIN = 1;
+    private static final double MINIMUM_FARE = 5.0;
     public double calculateFare(double distance, int time) {
-        return RATE_PER_KM * distance + RATE_PER_MIN * time;
+        double totalFare = RATE_PER_KM * distance + RATE_PER_MIN * time;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
