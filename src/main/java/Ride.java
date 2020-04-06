02925fbfee45a503d81caea039966edaecb5a5dd
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-06 21:18:05 +0530

    ADDED : Ride class to store details of every ride

--- src/main/java/Ride.java
+++ src/main/java/Ride.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Rides {
+public class Ride {
+    public double distance;
+    public int time;
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
 }
