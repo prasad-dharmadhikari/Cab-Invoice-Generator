2037ff6e626e030d3214c45e28335c91dd8f81aa
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-06 19:52:23 +0530

    ADDED : Test case to check the total fare of the journey

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFareForJourney() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 3.0;
+        int time = 10;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(40, fare, 0);
+    }
 }
