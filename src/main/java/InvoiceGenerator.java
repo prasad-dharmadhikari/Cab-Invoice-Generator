dbbeeee21909a00aa6b45f3bd77be2b55102382c
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-06 19:51:11 +0530

    ADDED : Method to calculate fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,5 +1,7 @@
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice problem");
+    private static final double RATE_PER_KM = 10;
+    private static final int RATE_PER_MIN = 1;
+    public double calculateFare(double distance, int time) {
+        return RATE_PER_KM * distance + RATE_PER_MIN * time;
     }
 }
