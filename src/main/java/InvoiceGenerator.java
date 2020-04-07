a9b6e31f50050328497366d9c094a3a576c2045b
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-07 19:07:54 +0530

    REFACTORED : Method to obtain list of rides by user ID from Ride repository and to return the invoice summary

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class InvoiceGenerator {
     private static final double RATE_PER_KM = 10;
     private static final int RATE_PER_MIN = 1;
     private static final double MINIMUM_FARE = 5.0;
+    RideRepository rideRepository = new RideRepository();
+    ArrayList<Ride> listOfRides = new ArrayList<Ride>();
 
     public double calculateFare(double distance, int time) {
         double totalFare = RATE_PER_KM * distance + RATE_PER_MIN * time;
@@ -11,8 +15,22 @@ public class InvoiceGenerator {
     public double calculateFareForMultipleRides(Ride[] rides) {
         double aggregateFare = 0;
         for (Ride ride : rides) {
+            listOfRides.add(ride);
             aggregateFare = aggregateFare + calculateFare(ride.distance, ride.time);
         }
         return aggregateFare;
+    }
+
+    public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+        return new InvoiceSummary(rides.length, calculateFareForMultipleRides(rides));
+    }
+
+    public void addRides(String userId) {
+        rideRepository.addUserRides(userId,listOfRides);
+    }
+
+    public ArrayList<Ride> getRidesByUserId(String userId) {
+        ArrayList<Ride> ridesByUserId = rideRepository.getRidesByUserId(userId);
+        return ridesByUserId;
     }
 }
