27a824f3c6bfb10793387ea38ce1f7b3c1700646
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-07 19:08:36 +0530

    ADDED : Class to maintain invoice summary

--- src/main/java/InvoiceSummary.java
+++ src/main/java/InvoiceSummary.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class InvoiceSummary {
+    public int noOfRides;
+    public double totalFare;
+    public double averageFarePerRide;
+
+    public InvoiceSummary(int noOfRides, double totalFare) {
+        this.noOfRides = noOfRides;
+        this.totalFare = totalFare;
+        this.averageFarePerRide = totalFare / noOfRides;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return noOfRides == that.noOfRides &&
+                Double.compare(that.totalFare, totalFare) == 0 &&
+                Double.compare(that.averageFarePerRide, averageFarePerRide) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceSummary -> {" +
+                "noOfRides=" + noOfRides +
+                ", totalFare=" + totalFare +
+                " Rs, averageFarePerRide=" + averageFarePerRide +
+                '}';
+    }
 }
