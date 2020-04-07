49803ed2fb37f5cf7b08c2bb4b5c803404611465
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-07 19:11:16 +0530

    ADDED : Test cases for returning Invoice summary and list of rides according to user ID

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -27,4 +27,25 @@ public class InvoiceGeneratorTest {
         double totalFare = invoiceGenerator.calculateFareForMultipleRides(rides);
         Assert.assertEquals(181, totalFare, 0);
     }
+
+    @Test
+    public void givenMultipleRideDetails_ShouldReturnInvoiceSummary() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(4.0, 15), new Ride(3.0, 11), new Ride(6.0, 25)};
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(rides);
+        InvoiceSummary expectedSummary = new InvoiceSummary(3,181.0);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
+    @Test
+    public void givenUserId_ShouldReturnListOfRidesAndInvoice() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        String userId= "a@b.com";
+        Ride[] rides = {new Ride(4.0, 15), new Ride(3.0, 11), new Ride(6.0, 25)};
+        invoiceGenerator.addRides(userId);
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(rides);
+        InvoiceSummary expectedSummary = new InvoiceSummary(3,181.0);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+        Assert.assertEquals(rides.length, invoiceGenerator.getRidesByUserId(userId).size());
+    }
 }
