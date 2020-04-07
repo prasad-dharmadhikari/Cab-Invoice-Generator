d61324336a26530806eb47915b803966d893ab40
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-07 19:10:22 +0530

    ADDED : Class to store rides of a user

--- src/main/java/RideRepository.java
+++ src/main/java/RideRepository.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RideRepository {
+    HashMap<String, ArrayList<Ride>> userRides = new HashMap<>();
+    public void addUserRides(String userId, ArrayList<Ride> listOfRides) {
+        userRides.put(userId, listOfRides);
+    }
+
+    public ArrayList<Ride> getRidesByUserId(String userId) {
+        ArrayList<Ride> rides = userRides.get(userId);
+        return rides;
+    }
 }
